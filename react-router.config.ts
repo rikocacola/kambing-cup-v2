09a205d9d972193ee435a22d2780e2290d11b055
Riko Chair <riko.chair@gmail.com>
2025-12-23 07:39:46 +0700

    feat: change docker file

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: false,
+  ssr: true,
   future: {
     v8_middleware: true,
   },
